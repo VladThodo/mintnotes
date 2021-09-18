fb8093adf5b36e528f8a8c15dd0212493d9a774f
VladThodo <vladtodosin@gmail.com>
2021-09-18 15:21:27 +0300

    Create NotesFragment

---
+++ app/src/main/java/com/todo/mintnotes/fragments/NotesFragment.java
@@ -0,0 +1,13 @@
+package com.todo.mintnotes.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.todo.mintnotes.R;
+
+public class NotesFragment extends Fragment {
+
+    public NotesFragment(){
+        super(R.layout.notes_fragment);
+    }
+
+}
