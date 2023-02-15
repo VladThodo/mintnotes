1082e0f3e784a92c68183b00618ccc785d2bada6
todo <vladtodosin@gmail.com>
2023-02-15 18:03:48 +0200

    Quick update and about fragment improvement

--- app/src/main/java/com/todo/mintnotes/fragments/AboutFragment.java
+++ app/src/main/java/com/todo/mintnotes/fragments/AboutFragment.java
@@ -1,36 +1,47 @@
 package com.todo.mintnotes.fragments;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.danielstone.materialaboutlibrary.MaterialAboutFragment;
-import com.danielstone.materialaboutlibrary.items.MaterialAboutTitleItem;
-import com.danielstone.materialaboutlibrary.model.MaterialAboutCard;
-import com.danielstone.materialaboutlibrary.model.MaterialAboutList;
 import com.todo.mintnotes.R;
+import com.vansuita.materialabout.builder.AboutBuilder;
+import com.vansuita.materialabout.views.AboutView;
 
-public class AboutFragment extends MaterialAboutFragment {
+public class AboutFragment extends Fragment {
 
-    public AboutFragment(){
-        super();
-    }
-
+    @Nullable
     @Override
-    @NonNull
-    protected MaterialAboutList getMaterialAboutList(@NonNull Context context) {
-        MaterialAboutCard.Builder cardBuilder = new MaterialAboutCard.Builder();
-        MaterialAboutTitleItem titleItem = new MaterialAboutTitleItem.Builder()
-                .text("Mintnotes")
-                .icon(R.drawable.ic_launcher_foreground)
-                .build();
-        cardBuilder.addItem(titleItem);
-        MaterialAboutCard card = cardBuilder.build();
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return new MaterialAboutList.Builder()
-                .addCard(card)
+        AboutView mview = AboutBuilder.with(this.getContext())
+                .setPhoto(R.mipmap.profile_picture)
+                .setCover(R.mipmap.profile_cover)
+                .setName("t0d0")
+                .setSubTitle("Random nerd")
+                .setBrief("I make stuff")
+                .setAppIcon(R.mipmap.ic_launcher)
+                .setAppName(R.string.app_name)
+                .addGooglePlayStoreLink("8002078663318221363")
+                .addGitHubLink("user")
+                .addFacebookLink("user")
+                .addFiveStarsAction()
+                .setVersionNameAsAppSubTitle()
+                .addShareAction(R.string.app_name)
+                .setWrapScrollView(true)
+                .setLinksAnimated(true)
+                .setShowAsCard(true)
                 .build();
+
+        mview.setPadding(16, 16, 16, 16);
+
+        return mview;
     }
 }
 
