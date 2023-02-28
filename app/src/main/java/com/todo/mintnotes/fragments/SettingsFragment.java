f3fe1ef8aa7f7c31ba07ef2c746a5b9dea67d2d5
todo <vladtodosin@gmail.com>
2023-02-28 10:27:08 +0200

    Preferences

--- app/src/main/java/com/todo/mintnotes/fragments/SettingsFragment.java
+++ app/src/main/java/com/todo/mintnotes/fragments/SettingsFragment.java
@@ -1,13 +1,22 @@
 package com.todo.mintnotes.fragments;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
 
 import com.todo.mintnotes.R;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     public SettingsFragment(){
-        super(R.layout.settings_fragment);
+
+    }
+
+    @Override
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+        setPreferencesFromResource(R.xml.settings_fragment, rootKey);
     }
 
 }
